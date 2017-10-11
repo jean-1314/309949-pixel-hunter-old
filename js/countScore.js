2b747608aa6f959713b8d3c00c1573dc80df386e
Савитский Иван Васильевич <isavitskiy@phonexit.ru>
2017-10-11 18:07:29 +0300

    Add countscore & test

---
+++ js/countScore.js
@@ -0,0 +1,48 @@
+let playerAnswers = [
+  {
+    answer: correct,
+    time: normal
+  },
+  {
+    answer: incorrect,
+    time: fast
+  },
+  {
+    answer: correct,
+    time: normal
+  },
+  {
+    answer: incorrect,
+    time: fast
+  },
+  {
+    answer: correct,
+    time: slow
+  },
+  {
+    answer: correct,
+    time: fast
+  },
+  {
+    answer: incorrect,
+    time: normal
+  },
+  {
+    answer: correct,
+    time: fast
+  },
+  {
+    answer: correct,
+    time: normal
+  },
+  {
+    answer: incorrect,
+    time: fast
+  }
+];
+let playerLives = 3;
+
+const countScore = (answers, lives) => {
+  let score = 0;
+
+};
