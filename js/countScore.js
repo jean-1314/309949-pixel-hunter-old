70a70107a17cd8d53a515f7d4edb1c69e260883c
jean-1314 <isfiremsk@gmail.com>
2017-10-11 22:32:47 +0300

    Update countscore

--- js/countScore.js
+++ js/countScore.js
@@ -1,48 +1,67 @@
 let playerAnswers = [
   {
-    answer: correct,
-    time: normal
+    answer: `correct`,
+    time: `normal`
   },
   {
-    answer: incorrect,
-    time: fast
+    answer: `incorrect`,
+    time: `fast`
   },
   {
-    answer: correct,
-    time: normal
+    answer: `correct`,
+    time: `normal`
   },
   {
-    answer: incorrect,
-    time: fast
+    answer: `incorrect`,
+    time: `fast`
   },
   {
-    answer: correct,
-    time: slow
+    answer: `correct`,
+    time: `slow`
   },
   {
-    answer: correct,
-    time: fast
+    answer: `correct`,
+    time: `fast`
   },
   {
-    answer: incorrect,
-    time: normal
+    answer: `incorrect`,
+    time: `normal`
   },
   {
-    answer: correct,
-    time: fast
+    answer: `correct`,
+    time: `fast`
   },
   {
-    answer: correct,
-    time: normal
+    answer: `correct`,
+    time: `normal`
   },
   {
-    answer: incorrect,
-    time: fast
+    answer: `incorrect`,
+    time: `fast`
   }
 ];
 let playerLives = 3;
 
 const countScore = (answers, lives) => {
   let score = 0;
-
+  for (const index in answers) {
+    if (answers.hasOwnProperty(index)) {
+      let value = answers[index];
+      switch (value) {
+        case `correct`:
+          score = score + 100;
+          break;
+        case `fast`:
+          score = score + 50;
+          break;
+        case `slow`:
+          score = score - 50;
+          break;
+      }
+    }
+  }
+  score = lives * 50;
+  return score;
 };
+
+countScore(playerAnswers, playerLives);
