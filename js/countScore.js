8f4019fa12d546f2225054707d766c82ba7cc52d
Савитский Иван Васильевич <isavitskiy@phonexit.ru>
2017-10-13 11:58:53 +0300

    Update function

--- js/countScore.js
+++ js/countScore.js
@@ -1,49 +1,49 @@
 let playerAnswers = [
   {
     answer: `correct`,
-    time: 4900
+    time: 24900
+  },
+  {
+    answer: `correct`,
+    time: 1900
   },
   {
     answer: `incorrect`,
-    time: 2700
+    time: 24900
   },
   {
     answer: `correct`,
-    time: 9200
+    time: 1900
+  },
+  {
+    answer: `correct`,
+    time: 24900
   },
   {
     answer: `incorrect`,
-    time: 3500
+    time: 1900
   },
   {
     answer: `correct`,
-    time: 10800
-  },
-  {
-    answer: `correct`,
-    time: 2200
+    time: 1900
   },
   {
     answer: `incorrect`,
-    time: 3500
-  },
-  {
-    answer: `correct`,
-    time: 1800
-  },
-  {
-    answer: `correct`,
-    time: 6800
+    time: 14900
   },
   {
     answer: `incorrect`,
-    time: 1100
+    time: 14900
+  },
+  {
+    answer: `correct`,
+    time: 14900
   }
 ];
-let playerLives = 3;
-let score = 0;
-const countScore = (answers, lives) => {
+let playerLives = 1;
 
+const countScore = (answers, lives) => {
+  let score = 0;
   for (const index in answers) {
     if (answers.hasOwnProperty(index)) {
       let value = answers[index];
@@ -52,19 +52,23 @@ const countScore = (answers, lives) => {
         score = score + 100;
       }
 
-      if (value.time < 3000) {
+      if (value.time < 10000) {
         score = score + 50;
       }
 
-      if (value.time > 10000) {
+      if (value.time > 20000) {
         score = score - 50;
       }
     }
   }
+
   score = score + (lives * 50);
+  if (answers.length < 10) {
+    score = -1;
+  }
   return score;
 };
 
 countScore(playerAnswers, playerLives);
 
-export {playerAnswers, playerLives, score, countScore};
+export {playerAnswers, playerLives, countScore};
