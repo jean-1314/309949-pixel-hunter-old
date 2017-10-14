b989e75d89ba1f0dde6cb03c975b54b0af562222
jean-1314 <isfiremsk@gmail.com>
2017-10-14 17:29:36 +0300

    Update timer test

--- js/data/timer.test.js
+++ js/data/timer.test.js
@@ -2,14 +2,7 @@ import {timer} from '../timer.js';
 import assert from 'assert';
 
 suite(`Test timer:`, () => {
-  test(`negative amount of seconds`, () => {
-    assert(timer(-5) === false);
-  });
-  test(`data type other than number`, () => {
-    assert(timer([1, 2, 3]) === false);
-    assert(timer({1: 1, 2: 2}) === false);
-    assert(timer(null) === false);
-    assert(timer(`thirty`) === false);
-    assert(timer(true) === false);
+  test(`time decreases`, () => {
+    assert(timer.tick() === true);
   });
 });
